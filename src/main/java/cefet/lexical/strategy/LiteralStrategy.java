c6c8ede5951e2d7c74806524bda26fd319759f24
PaulitoRenatito <paulorenatosouza02@gmail.com>
2024-11-18 22:39:21 -0300

    feat: add literal strategy

---
+++ src/main/java/cefet/lexical/strategy/LiteralStrategy.java
@@ -0,0 +1,31 @@
+package cefet.lexical.strategy;
+
+import cefet.lexical.Lexer;
+import cefet.lexical.token.Token;
+import cefet.lexical.token.TokenType;
+import cefet.lexical.token.Word;
+
+import static cefet.lexical.Lexer.EOF_UNICODE;
+
+public class LiteralStrategy implements TokenStrategy {
+    @Override
+    public Token identifyToken(Lexer lexer) {
+        if (lexer.getCurrentChar() == '{') {
+            StringBuilder sb = new StringBuilder();
+
+            do {
+                sb.append(lexer.getCurrentChar());
+                lexer.readch();
+                if (lexer.getCurrentChar() == EOF_UNICODE)
+                    return new Token(TokenType.ERROR);
+            } while (lexer.getCurrentChar() != '}');
+
+            sb.append(lexer.getCurrentChar());
+            lexer.readch();
+
+            return new Word(TokenType.LITERAL, sb.toString());
+        }
+
+        return null;
+    }
+}
