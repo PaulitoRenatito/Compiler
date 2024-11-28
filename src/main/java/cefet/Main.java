25dfb42287c30f89b441f245587dd72cc7e22ef1
GaTalim <gabriel.talim@gmail.com>
2024-11-27 21:08:29 -0300

    Fins de teste

--- src/main/java/cefet/Main.java
+++ src/main/java/cefet/Main.java
@@ -4,9 +4,20 @@ import cefet.lexical.Lexer;
 import cefet.lexical.token.Token;
 import cefet.lexical.token.TokenType;
 
+import java.io.File;
+
+
 public class Main {
     public static void main(String[] args) {
-        Lexer lexer = new Lexer(args[0]);
+        String path = "src/main/resources/examples/test1.txt";
+
+        File file = new File(path);
+        if (!file.exists()) {
+            System.err.println("File not found: " + path);
+            return;
+        }
+
+        Lexer lexer = new Lexer(file.getAbsolutePath());
         Token token;
 
         do {
